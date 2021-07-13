189e3377e865b0186e78587b59f503531f7b9b8d
JIK-K <87060676+JIK-K@users.noreply.github.com>
2021-07-13 17:56:04 +0900

    Add files via upload

---
+++ 나쁜팀/NumberBaseball/src/main/java/com/mycompany/numberbaseball/NumberBaseball.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.numberbaseball;
+
+/**
+ *
+ * @author ASUS
+ */
+public class NumberBaseball {
+    //------------------------------------------------------------------------//
+    //인스턴스 변수
+    
+    //------------------------------------------------------------------------//
+    //메서드
+    public static void main(String[] args){
+        GameManager gm = new GameManager(5);
+        gm.play();
+        
+    }
+    //------------------------------------------------------------------------//
+}
