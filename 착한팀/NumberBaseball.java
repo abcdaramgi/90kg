6d99b0d7a7d4f8811b1e5a1dd8703cf97fbfbcf8
dhkim1206 <69470424+dhkim1206@users.noreply.github.com>
2021-07-17 00:34:25 +0900

    분기점1

---
+++ 착한팀/NumberBaseball.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.numberbaseball;
+
+/**
+ *
+ * @author kdh12
+ */
+public class NumberBaseball {
+    //------------------------------------------------------------------------//
+    // 메서드
+    public static void main( String[] args){
+        GameManager gm = new GameManager();
+        gm.play();
+    }
+}
